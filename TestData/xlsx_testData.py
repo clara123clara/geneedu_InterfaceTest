821a014d6286793563c59888fd516b83e4f54beb
Administrator <Administrator@chenjiajia>
2017-10-23 21:58:09 +0800

    q

---
+++ TestData/xlsx_testData.py
@@ -0,0 +1,24 @@
+'''
+Created on 2017年10月23日
+
+@author: Administrator
+'''
+import xlrd
+
+xlsurl="E:\\EclipseWorkspace\\geneedu_InterfaceTest\\TestData" #表格文件路径
+filexls_obj=xlrd.open_workbook(xlsurl) #获取文件对象
+
+sheetName=filexls_obj.sheet_names()[0]#获取表格第一个sheet的名称
+sheet_obj=filexls_obj.sheet_by_name(sheetName)#获取第一个sheet的对象
+
+total_rows=sheet_obj.nrows #获取表格总行数
+total_cols=sheet_obj.ncols #获取表格总列数
+
+cols_data=sheet_obj.col_values(0) #获取表格第一列的数据
+rows_data=sheet_obj.row_values(1) #获取表格第二行的数据
+
+print("=========================================")
+print(cols_data)
+print(rows_data)
+
+
